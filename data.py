42edad2e519d57b170d5e6fc6c9065d0a6a1f5c7
nehalsawant12 <105156423+nehalsawant12@users.noreply.github.com>
2022-05-11 17:08:22 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'For, each man can do best and excel in only that thing of which he is passionately fond, in which he believes, as I do, that he has the ability to do it, that he is in fact born and destined to do it.',
+            'author':'Homi Jehangir Bhabha',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Never stop fighting until you arrive at your destined place — that is, the unique you. Have an aim in life, continuously acquire knowledge, work hard, and have perseverance to realize the great life.',
+            'author':'Dr APJ Abdul Kalam',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Success can come to you by courageous devotion to the task lying in front of you.”',
+            'author':'Sir CV Raman',
+            'create_date':'04-25-2017'
+        }
+    ]
+    return articles
